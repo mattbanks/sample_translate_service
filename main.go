090ca78f3b1cc0545ba3e1ef91fede5664acd457
Matt Banks <mjbanks@gmail.com>
2016-07-29 14:32:06 -0700

    setup message struct and translate POST endpoint

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
-package translateservice
+package main
 
 import (
-	"fmt"
-	"log"
-	"io"
 	"net/http"
+	"log"
+	"time"
 	"github.com/emicklei/go-restful"
 )
 
-func main() {
-	ws := new(restful.WebService)
-	ws.Route(ws.GET("/hello").To(hello))
-	restful.Add(ws)
-	http.ListenAndServe(":8080", nil)
+type Message struct {
+	Id 			int 		`json:"id"`
+	Language 	string 		`json:"language"`
+    Content 	string 		`json:"content"`
+    Translated 	time.Time 	`json:"translated_on"`
 }
 
-func hello(req *restful.Request, resp *restful.Response) {
-	io.WriteString(resp, "world")
+func main() {
+	ws := new(restful.WebService)
+	ws.Route(ws.POST("/translate").Consumes("application/json").To(translate))
+	restful.Add(ws)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func translate(req *restful.Request, resp *restful.Response,) {
+	message := new(Message)
+
+	if err := req.ReadEntity(message); err != nil {
+		resp.WriteErrorString(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	message.Translated = time.Now()
+
+	resp.WriteAsJson(message)
 }
